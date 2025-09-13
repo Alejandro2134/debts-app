fc5cef24cd4319d5f8b02a79c58bb87a98eecfa6
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2025-09-13 14:18:20 -0500

    feat: added global middleware to catch errors

---
+++ backend/src/infrastructure/web/frameworks/nest/middlewares/HttpExceptionFilter.ts
@@ -0,0 +1,47 @@
+import { ConflictError } from '@application/errors/ConflictError';
+import { ForbiddenError } from '@application/errors/ForbiddenError';
+import { NotFoundError } from '@application/errors/NotFoundError';
+import { PersistencyError } from '@application/errors/PersistencyError';
+import { UnauthorizedError } from '@application/errors/UnauthorizedError';
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message = 'Internal server error';
+
+    if (exception instanceof NotFoundError) {
+      status = HttpStatus.NOT_FOUND;
+      message = exception.message;
+    } else if (exception instanceof PersistencyError) {
+      status = HttpStatus.INTERNAL_SERVER_ERROR;
+      message = exception.message;
+    } else if (exception instanceof ConflictError) {
+      status = HttpStatus.CONFLICT;
+      message = exception.message;
+    } else if (exception instanceof UnauthorizedError) {
+      status = HttpStatus.UNAUTHORIZED;
+      message = exception.message;
+    } else if (exception instanceof ForbiddenError) {
+      status = HttpStatus.FORBIDDEN;
+      message = exception.message;
+    } else if (exception instanceof Error) {
+      message = exception.message;
+    }
+
+    response.status(status).json({
+      statusCode: status,
+      message,
+    });
+  }
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@infrastructure/web/frameworks/nest/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from '@infrastructure/web/frameworks/nest/middlewares/HttpExceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
